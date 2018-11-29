7f2a5ebe6becb219884d061c8a188ebf6e0ae877
Eric Robertson <ericrobertson@Erics-MacBook-Pro.local>
2018-11-29 13:01:56 -0500

    Update SHA

--- maskgen.rb
+++ maskgen.rb
@@ -1,8 +1,8 @@
 class Maskgen < Formula
   desc "Maskgen JT"
   homepage "https://github.com/rwgdrummer/maskgen"
-  url "https://github.com/rwgdrummer/maskgen_installer/archive/1.0.tar.gz"
-  sha256 "c4bf331a7d0ca1bdd0bc7f389d8ab667eb4620fb544711b56e68507cfd17a0e9"
+  url "https://github.com/rwgdrummer/maskgen_installer/archive/1.0a.tar.gz"
+  sha256 "38b5254f3e900efb53484ba35c83d4806e377a2439cb54fbaab1216cd5adc815"
   head "https://github.com/rwgdrummer/maskgen.git"
 
   depends_on "cmake" => :build
